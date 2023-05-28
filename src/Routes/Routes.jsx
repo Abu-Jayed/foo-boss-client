47e03f1e2997f38bdefbbb7706ea2ed9beb553b7
abu-jayed <aj.webdevlopment@gmail.com>
2023-05-28 11:41:01 +0600

    initial commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,44 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+import Menu from "../Pages/Menu/Menu/Menu";
+import Order from "../Pages/Order/Order/Order";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
+import Secret from "../Pages/Shared/Secret/Secret";
+import PrivetRoute from "./PrivetRoute";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+          path: 'menu',
+          element: <Menu></Menu>
+        },
+        {
+          path: 'order/:category',
+          element: <Order></Order>
+        },
+        {
+          path: 'login',
+          element: <Login></Login>
+        },
+        {
+          path: 'signup',
+          element: <SignUp></SignUp>
+        },
+        {
+          path: 'secret',
+          element: <PrivetRoute><Secret></Secret></PrivetRoute>
+        }
+      ]
+    },
+  ]);
